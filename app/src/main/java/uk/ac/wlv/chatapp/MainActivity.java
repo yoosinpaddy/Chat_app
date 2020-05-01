b307b581a3b1f8ab2f480d093bdecd7d6fb6540b
karumajohn <karumajohn252203@gmail.com>
2020-05-01 21:41:06 +0300

    Initial commit

---
+++ app/src/main/java/uk/ac/wlv/chatapp/MainActivity.java
@@ -0,0 +1,19 @@
+package uk.ac.wlv.chatapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.ComponentName;
+import android.content.ContentProvider;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
